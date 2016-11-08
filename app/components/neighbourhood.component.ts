4dde895231c21e930faaa82ef7d860967d0d7820
farhanahmedsyed <farhanahmedsyed@gmail.com>
2016-11-07 18:07:12 -0600

    Added new NeighbourhoodComponent
    template and Css is still left to implement code is broken

---
+++ app/components/neighbourhood.component.ts
@@ -0,0 +1,36 @@
+import {Component,OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+import {Neighbourhood} from '../model/neighbourhood';
+import {NeighbourhoodService} from '../services/neighbourhood.service';
+
+@Component({
+    moduleId:module.id,
+    selector:'my-neighbourhood',
+    templateUrl:'',
+    styleUrls:['']
+})
+
+export class NeighbourhoodComponent implements OnInit{
+
+    neighbourhoods: Neighbourhood[];
+    selectedNeighbourhood: Neighbourhood;
+
+    constructor(
+        private router: Router,
+        private nService:NeighbourhoodService
+    ){}
+
+    getNeighbourhoods(): void{
+        this.nService.getNeighbourhoods().then(nh => this.neighbourhoods = nh)
+    }
+
+    ngOnInit():void{
+        this.getNeighbourhoods();
+    }
+
+    onSelect(neighbourhood:Neighbourhood):void{
+        this.selectedNeighbourhood = neighbourhood;
+    }
+}
